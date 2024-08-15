394c36e7295ff7466b86ca0cb2a07800f797c61d
Yuri Edward <yuri6037@outlook.com>
2024-08-15 15:17:40 +0200

    Added BitsTests with bits protocol

--- Swift/testprogTests/BitsTests.swift
+++ Swift/testprogTests/BitsTests.swift
@@ -1,35 +1,65 @@
+// Copyright (c) 2024, BlockProject 3D
 //
-//  BitsTests.swift
-//  testprogTests
+// All rights reserved.
 //
-//  Created by Yuri Edward on 8/15/24.
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
 //
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of BlockProject 3D nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 import XCTest
+import testprog
 
 final class BitsTests: XCTestCase {
-
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
+    func testNumbers() throws {
+        let nums = Numbers();
+        XCTAssertEqual(SIZE_NUMBERS, 4);
+        nums.setA(-8).setB(15).setC(-65536).setD(127);
+        XCTAssertEqual(nums.a, -8);
+        XCTAssertEqual(nums.b, 15);
+        XCTAssertEqual(nums.c, -65536);
+        XCTAssertEqual(nums.d, 127);
+        nums.setC(65535);
+        XCTAssertEqual(nums.c, 65535);
+        nums.setA(-7);
+        XCTAssertEqual(nums.a, -7);
+        XCTAssertEqual(nums.rawA, 9);
+        nums.setA(-6);
+        XCTAssertEqual(nums.a, -6);
+        XCTAssertEqual(nums.rawA, 10);
+        nums.setA(-5);
+        XCTAssertEqual(nums.a, -5);
+        XCTAssertEqual(nums.rawA, 11);
+        nums.setA(1);
+        XCTAssertEqual(nums.a, 1);
+        XCTAssertEqual(nums.rawA, 1);
+        nums.setA(4);
+        XCTAssertEqual(nums.a, 4);
+        XCTAssertEqual(nums.rawA, 4);
     }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
