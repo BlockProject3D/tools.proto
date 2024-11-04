29871ac90c5b09e3b5d8fb097a518da56ed80797
Yuri Edward <yuri6037@outlook.com>
2024-11-04 18:45:19 +0100

    Added tests for bad message formatting

---
+++ compiler/tests/test_bad_message.rs
@@ -0,0 +1,162 @@
+// Copyright (c) 2024, BlockProject 3D
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of BlockProject 3D nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+use bp3d_protoc::api::core::Error;
+use bp3d_protoc::api::core::loader::Loader;
+use bp3d_protoc::gen::RustImportSolver;
+
+const VALUE_ITEM_TYPE_SOME: &str = "
+{
+    name: \"test\",
+    messages: [
+        {
+            name: \"Test\",
+            fields: [
+                { name: \"v\", value: { type: \"unsigned\", bits: 32 }, item_type: \"test\" }
+            ]
+        }
+    ]
+}
+";
+
+const VALUE_ITEM_TYPE_NONE: &str = "
+{
+    name: \"test\",
+    messages: [
+        {
+            name: \"Test\",
+            fields: [
+                { name: \"v\" }
+            ]
+        }
+    ]
+}
+";
+
+const LIST_NO_LEN: &str = "
+{
+    name: \"test\",
+    messages: [
+        {
+            name: \"Test\",
+            fields: [
+                { name: \"v\", value: { type: \"list\", item_type: \"test\", max_len: 0 } }
+            ]
+        }
+    ]
+}
+";
+
+const STRING_NO_LEN: &str = "
+{
+    name: \"test\",
+    messages: [
+        {
+            name: \"Test\",
+            fields: [
+                { name: \"v\", value: { type: \"string\", max_len: 0 } }
+            ]
+        }
+    ]
+}
+";
+
+const SIZED_LIST_NO_SIZE: &str = "
+{
+    name: \"test\",
+    messages: [
+        {
+            name: \"Test1\",
+            fields: [
+                { name: \"v\", value: { type: \"string\", max_len: 123 } }
+            ]
+        },
+        {
+            name: \"Test\",
+            fields: [
+                { name: \"v\", value: { type: \"list\", item_type: \"Test1\", max_len: 123, max_size: 0 } }
+            ]
+        }
+    ]
+}
+";
+
+#[test]
+fn value_item_type_some() {
+    let mut loader = Loader::new(1);
+    loader.load_from_string(VALUE_ITEM_TYPE_SOME, "").unwrap();
+    let err = loader.compile(&RustImportSolver).unwrap_err();
+    assert!(matches!(
+        err,
+        Error::Compiler(bp3d_protoc::compiler::Error::BadFieldType)
+    ));
+}
+
+#[test]
+fn value_item_type_none() {
+    let mut loader = Loader::new(1);
+    loader.load_from_string(VALUE_ITEM_TYPE_NONE, "").unwrap();
+    let err = loader.compile(&RustImportSolver).unwrap_err();
+    assert!(matches!(
+        err,
+        Error::Compiler(bp3d_protoc::compiler::Error::BadFieldType)
+    ));
+}
+
+#[test]
+fn list_no_len() {
+    let mut loader = Loader::new(1);
+    loader.load_from_string(LIST_NO_LEN, "").unwrap();
+    let err = loader.compile(&RustImportSolver).unwrap_err();
+    assert!(matches!(
+        err,
+        Error::Compiler(bp3d_protoc::compiler::Error::ZeroArray)
+    ));
+}
+
+#[test]
+fn string_no_len() {
+    let mut loader = Loader::new(1);
+    loader.load_from_string(STRING_NO_LEN, "").unwrap();
+    let err = loader.compile(&RustImportSolver).unwrap_err();
+    assert!(matches!(
+        err,
+        Error::Compiler(bp3d_protoc::compiler::Error::ZeroArray)
+    ));
+}
+
+#[test]
+fn sized_list_no_size() {
+    let mut loader = Loader::new(1);
+    loader.load_from_string(SIZED_LIST_NO_SIZE, "").unwrap();
+    let err = loader.compile(&RustImportSolver).unwrap_err();
+    assert!(matches!(
+        err,
+        Error::Compiler(bp3d_protoc::compiler::Error::ZeroArray)
+    ));
+}
