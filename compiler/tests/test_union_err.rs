a89d6d6aa3a996ca7487dadbd8461b127366a167
Yuri Edward <yuri6037@outlook.com>
2024-11-04 21:28:13 +0100

    Added tests for union errors

---
+++ compiler/tests/test_union_err.rs
@@ -0,0 +1,75 @@
+// Copyright (c) 2024, BlockProject 3D
+//
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without modification,
+// are permitted provided that the following conditions are met:
+//
+//     * Redistributions of source code must retain the above copyright notice,
+//       this list of conditions and the following disclaimer.
+//     * Redistributions in binary form must reproduce the above copyright notice,
+//       this list of conditions and the following disclaimer in the documentation
+//       and/or other materials provided with the distribution.
+//     * Neither the name of BlockProject 3D nor the names of its contributors
+//       may be used to endorse or promote products derived from this software
+//       without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+// "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+// LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+// A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+// EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+// PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+use bp3d_protoc::api::core::Error;
+use bp3d_protoc::api::core::loader::Loader;
+use bp3d_protoc::gen::RustImportSolver;
+
+const TEST_UNION_BAD_DISCRIMINANT: &str = include_str!("test_union_bad_discriminant.json5");
+const TEST_UNION_BAD_DISCRIMINANT_2: &str = include_str!("test_union_bad_discriminant_2.json5");
+const TEST_UNION_UNDEFINED_DISCRIMINANT: &str = include_str!("test_union_undefined_discriminant.json5");
+
+#[test]
+fn bad_discriminant() {
+    let mut loader = Loader::new(1);
+    loader.load_from_string(TEST_UNION_BAD_DISCRIMINANT, "").unwrap();
+    let err = loader.compile(&RustImportSolver).unwrap_err();
+    assert!(matches!(
+        err,
+        Error::Compiler(bp3d_protoc::compiler::Error::UnionTypeMismatch)
+    ));
+}
+
+#[test]
+fn bad_discriminant_2() {
+    let mut loader = Loader::new(1);
+    loader.load_from_string(TEST_UNION_BAD_DISCRIMINANT_2, "").unwrap();
+    let err = loader.compile(&RustImportSolver).unwrap_err();
+    assert!(matches!(
+        err,
+        Error::Compiler(bp3d_protoc::compiler::Error::UnionTypeMismatch)
+    ));
+}
+
+#[test]
+fn undefined_discriminant() {
+    let mut loader = Loader::new(1);
+    loader.load_from_string(TEST_UNION_UNDEFINED_DISCRIMINANT, "").unwrap();
+    let err = loader.compile(&RustImportSolver).unwrap_err();
+    match err {
+        Error::Compiler(v) => match v {
+            bp3d_protoc::compiler::Error::UndefinedReference(v) => {
+                assert_eq!(v, "undefined");
+                return;
+            }
+            _ => (),
+        },
+        _ => (),
+    }
+    assert!(false);
+}
