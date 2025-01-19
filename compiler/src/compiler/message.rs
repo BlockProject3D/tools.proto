cfe9bc23883c3fcb1cbe76acb1b8f8ec5d184111
Yuri Edward <yuri6037@outlook.com>
2025-01-19 16:54:30 +0100

    Fixed bug with structures containing arrays

--- compiler/src/compiler/message.rs
+++ compiler/src/compiler/message.rs
@@ -455,7 +455,7 @@ impl Field {
             let r = Referenced::lookup(proto, &item_type).ok_or(Error::UndefinedReference(item_type))?;
             match r {
                 Referenced::Struct(r) => {
-                    let is_single = r.fields.len() == 1;
+                    let is_single = r.fields.len() == 1 && r.fields[0].loc.byte_size <= 8;
                     let is_fixed = r.fields[0].ty.as_fixed().is_some();
                     let is_none = r.fields[0].ty.as_fixed().map(|v| v.raw.is_none()).unwrap_or_default();
                     let is_byte_aligned = r.fields[0].loc.bit_size % 8 == 0;
